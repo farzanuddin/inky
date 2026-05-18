b3e2f872566d81a1c11a0474e3ae436e35c9059f
Farzan Uddin <m.farzanuddin@gmail.com>
2026-05-18 11:38:07 +0400

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/inky/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
